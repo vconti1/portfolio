57ef854c298a9e273fb4b7942a11fdabceb619b9
vconti1 <vincentconti393@gmail.com>
2025-03-31 18:27:34 -0400

    Finish hero section and begin implementation for bento grid

---
+++ data/index.ts
@@ -0,0 +1,5 @@
+export const gridItems = [
+    {title: 'Title1', description: 'Desc1', id: 1},
+    {title: 'Title1', description: 'Desc1', id: 1},
+    {title: 'Title1', description: 'Desc1', id: 1},
+]
