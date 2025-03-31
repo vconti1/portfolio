3fbbbfe81b95cb4eff5b12bcabc6308a96020ee3
vconti1 <vincentconti393@gmail.com>
2025-03-31 16:52:22 -0400

    correct small typo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,14 +18,15 @@ const Hero = () => {
         )}
       />
       {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"/>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] 
+      dark:bg-black"/>
 
       <div className="flex flex-col justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flec-col items-center justify-center">
-            
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center max-w-80"> Dynamic Web Magic with Next.js</h2>
             <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg: text-6xl"
-            words="Vincent Conti's Portfolio"
+            words="Transforming Concepts into Seamless Experiences"
             />
             </div>
         </div>
