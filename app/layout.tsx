549625d24d4edb9123a2552a76a8ee0c9438a708
vconti1 <vincentconti393@gmail.com>
2025-04-17 13:17:23 -0400

    update page page title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Vincent's Portfolio",
-  description: "Whatsup",
+  title: "Vincent Conti | Full-Stack Engineer",
+  description: "Projects and experience in full-stack development.",
 };
 
 export default function RootLayout({
